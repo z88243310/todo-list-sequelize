4e68488a8801a0496314c016ef118bb689ace027
z88243310 <z88243310@gmail.com>
2022-01-24 17:22:23 +0800

    feat: add login/register router

---
+++ routes/modules/users.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+  res.send('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  res.send('register')
+})
+
+router.get('/logout', (req, res) => {
+  res.send('logout')
+})
+
+module.exports = router
