da200cbbee07fc4059f6b2fbf037e856da1a469f
z88243310 <z88243310@gmail.com>
2022-01-25 17:10:20 +0800

    faet: add edit page and save function with not empty value

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -5,10 +5,37 @@ const db = require('../../models')
 const Todo = db.Todo
 
 // edit page
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const userId = req.user.id
+  return Todo.findOne({ where: { id, userId }, raw: true, nest: true })
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => res.status(422).json(error))
+})
 
 // put edit
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const userId = req.user.id
+  const { name, isDone } = req.body
+  // 不能為空值
+  if (name === '') {
+    return res.redirect(`/todos/${id}`)
+  }
+  return Todo.findOne({ where: { id, userId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => res.status(422).json(error))
+})
 
 // create page
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
 
 // post create
 
