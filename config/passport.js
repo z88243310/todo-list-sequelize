d0f7af6c1c3c6df2cf9af69b1dee05c02c1191bb
z88243310 <z88243310@gmail.com>
2022-01-28 11:23:49 +0800

    feat: add passport-facebook strategy

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,19 @@
 const passport = require('passport')
+// 引用 本地 登入策略
 const LocalStrategy = require('passport-local').Strategy
+// 引用 Facebook 登入策略
+const FacebookStrategy = require('passport-facebook').Strategy
+
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 
 module.exports = app => {
+  // 初始化 passport 模組
   app.use(passport.initialize())
   app.use(passport.session())
+
+  // 設定本地登入策略
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
       User.findOne({ where: { email } })
@@ -24,6 +31,38 @@ module.exports = app => {
         .catch(err => done(err, false))
     })
   )
+  // 設定 Facebook 登入策略
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName']
+      },
+      (accessToken, refreshToken, profile, done) => {
+        const { name, email } = profile._json
+        User.findOne({ where: { email } }).then(user => {
+          if (user) return done(null, user)
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash =>
+              User.create({
+                name,
+                email,
+                password: hash
+              })
+            )
+            .then(user => done(null, user))
+            .catch(error => done(error, false))
+        })
+      }
+    )
+  )
+
+  // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const { authenticator } = require('../middleware/auth')
 
 const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
+const auth = require('./modules/auth')
+
+// 掛載 middleware
+const { authenticator } = require('../middleware/auth')
 
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
